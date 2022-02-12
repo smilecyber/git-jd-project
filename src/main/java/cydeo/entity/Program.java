51f5564903056855eda01e934d31b24449b20283
smilecyber <ismail.yildirim@cybertekschool.com>
2022-02-12 21:44:37 +0300

    program features updated

--- src/main/java/cydeo/entity/Program.java
+++ src/main/java/cydeo/entity/Program.java
@@ -2,6 +2,8 @@ package cydeo.entity;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class Program {
     private Integer id;
@@ -9,6 +11,11 @@ public class Program {
     private String programName;
     //some work here
     private String programDetails;
+    private UUID externalProgramId;
     private String published;
+
+    public String retrieveProgramDetails(){
+        return programCode + " " + programDetails;
+    }
 }
 
